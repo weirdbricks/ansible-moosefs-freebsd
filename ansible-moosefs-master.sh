d1073ddb6f1be6bb3fc30724fc5ad355c9d1ad05
Lampros <weirdbricks@myopera.com>
2015-03-27 22:49:40 -0700

    Update ansible-moosefs-master.sh

--- ansible-moosefs-master.sh
+++ ansible-moosefs-master.sh
@@ -27,8 +27,7 @@ mkdir -p /usr/local/etc/ansible || notify
 fetch -q https://github.com/weirdbricks/ansible-moosefs-freebsd/raw/master/hosts -o /usr/local/etc/ansible/hosts --no-verify-peer || notify
 fetch -q https://github.com/weirdbricks/ansible-moosefs-freebsd/raw/master/mfs-build.yml --no-verify-peer || notify
 fetch -q https://github.com/weirdbricks/ansible-moosefs-freebsd/raw/master/mfs-master.yml --no-verify-peer || notify
-fetch -q https://github.com/weirdbricks/ansible-moosefs-freebsd/raw/master/mfs-chunkhosts.yml --no-verify-peer || notify
 echo "Done downloading files -- starting Ansible" 
 sleep 2 && clear
 ansible-playbook mfs-master.yml
-echo "Done setting up the master, now bootstraping pkg and installing Python on Chunkservers"
+echo "---replace----me----"
