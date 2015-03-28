f7b99b3e2d14fe208a0e1b3be918c67b354bf3d4
Lampros <weirdbricks@myopera.com>
2015-03-27 22:25:48 -0700

    Update ansible-moosefs-master.sh

--- ansible-moosefs-master.sh
+++ ansible-moosefs-master.sh
@@ -31,3 +31,5 @@ fetch -q https://github.com/weirdbricks/ansible-moosefs-freebsd/raw/master/mfs-c
 echo "Done downloading files -- starting Ansible" 
 sleep 2 && clear
 ansible-playbook mfs-master.yml
+echo "Done setting up the master, now bootstraping pkg and installing Python on Chunkservers"
+ansible mfs-chunkservers -m raw -a 'env ASSUME_ALWAYS_YES=YES pkg bootstrap -f; pkg install python'
