1df44dab478036cbf5c4d67519adc9bb02bcc75c
Lampros <weirdbricks@myopera.com>
2015-03-27 23:29:54 -0700

    Update ansible-moosefs-chunkservers.sh

--- ansible-moosefs-chunkservers.sh
+++ ansible-moosefs-chunkservers.sh
@@ -26,4 +26,8 @@ else
 endif
 
 echo "Bootstrapping pkg and python on chunkservers" 
-ansible mfs-chunkservers -m raw -a 'env ASSUME_ALWAYS_YES=YES pkg bootstrap -f; pkg install python'
+ansible mfs-chunkservers -m raw -a 'env ASSUME_ALWAYS_YES=YES pkg bootstrap -f'
+ansible mfs-chunkservers -m raw -a 'env ASSUME_ALWAYS_YES=YES pkg install python'
+echo "Downloading Ansible playbook for Chunkservers..."
+fetch -q -o - https://github.com/weirdbricks/ansible-moosefs-freebsd/raw/master/mfs-chunkhosts.yml --no-verify-peer
+ansible-playbook mfs-chunkhosts.yml
